d186983de56119a93a850e24898822e101d44c95
IbrahimG88 <goharyibrahim@gmail.com>
2021-06-16 14:09:24 +0200

    props and expressions

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class NavBar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github",
+    //alternative to the usual props, you can use only one of the two ways  <Navbar title="...".../>
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
