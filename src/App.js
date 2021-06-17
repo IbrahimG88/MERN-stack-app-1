4c554568d7039a1d1a5c6c93c852e07a0d9dc0e3
IbrahimG88 <goharyibrahim@gmail.com>
2021-06-17 15:23:16 +0200

    adding state to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./layout/NavBar";
+import UserItem from "./components/users/UserItem";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <NavBar title='Github Finder' icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
