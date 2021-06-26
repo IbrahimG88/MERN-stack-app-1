a7882ee09976c10aa5f79c699d64b1988b1bd9e3
IbrahimG88 <goharyibrahim@gmail.com>
2021-06-26 16:33:48 +0200

    Creating a functional class component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = (props) => {
+  const { login, avatar_url, html_url } = props.user;
 
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
 
-// now we are using this.props.user for destructuring instead of this.state since we are using props
+// making this class component into a functional component
+// changes: this.props will be props, since it is an argument now
--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class NavBar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-    //alternative to the usual props, you can use only one of the two ways  <Navbar title="...".../>
-  };
+const NavBar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+NavBar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+  //alternative to the usual props, you can use only one of the two ways  <Navbar title="...".../>
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default NavBar;
 
-// create the class components using the snippet rce
+// now make the class components into a functional component
