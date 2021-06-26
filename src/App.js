ed7e92073cbe20e8e1de3bc9a96453d56832ebd5
IbrahimG88 <goharyibrahim@gmail.com>
2021-06-26 16:19:43 +0200

    using axious for http requests showing the data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./layout/NavBar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    console.log(res.data);
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <NavBar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
@@ -17,3 +33,34 @@ class App extends Component {
 }
 
 export default App;
+
+//Used axios to get data from github api
+
+/* simplest form of axious get request:
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users")
+      .then((res) => console.log(res.data));
+  }
+
+  //Same as:
+
+   async componentDidMount() {
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+  }
+
+
+  // getting the data using axious and adding them to the state using setState:
+
+   async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    console.log(res.data);
+
+    this.setState({ users: res.data, loading: false });
+  }
+  */
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,50 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
@@ -59,3 +19,16 @@ const userStyle = {
   gridGap: "1rem",
 };
 export default Users;
+
+/*
+first we were getting the data from this components state, we will change that
+to get the data from the props from app.js and will substitute the this.state to this.props
+
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+
+                {this.props.users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+*/
