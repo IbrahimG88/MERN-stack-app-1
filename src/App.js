8dded1b28f05c51d8c1640e7fd6eac7bdd98f050
IbrahimG88 <goharyibrahim@gmail.com>
2021-07-06 16:15:03 +0200

    searchUsers

--- src/App.js
+++ src/App.js
@@ -23,12 +23,23 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    console.log(text);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <NavBar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
